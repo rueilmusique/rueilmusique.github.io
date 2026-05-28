270e69313700a511fd7517bb651adb33d76021f7
TryAgain911 <emulateurs@hotmail.fr>
2026-05-28 13:23:57 +0200

    analytics fix

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,63 +1,60 @@
-// Google Tag Manager
-(function(w,d,s,l,i){
-  w[l]=w[l]||[];
-  w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-  var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),
-      dl=l!='dataLayer'?'&l='+l:'';
-  j.async=true;
-  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-  f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PDWTVMM2');
-// End Google Tag Manager
+window.dataLayer = window.dataLayer || [];
+
+function gtag(){
+	dataLayer.push(arguments);
+}
 
 /*
-//Google Tag Manager - Nouveau script
+|--------------------------------------------------------------------------
+| Consentement par défaut = refusé
+|--------------------------------------------------------------------------
+*/
+
+gtag('consent', 'default', {
+	'ad_storage': 'denied',
+	'analytics_storage': 'denied',
+	'ad_user_data': 'denied',
+	'ad_personalization': 'denied'
+});
+
+/*
+|--------------------------------------------------------------------------
+| Google Tag Manager
+|--------------------------------------------------------------------------
+*/
+
 (function(w,d,s,l,i){
 	w[l]=w[l]||[];
-	w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+
+	w[l].push({
+		'gtm.start': new Date().getTime(),
+		event:'gtm.js'
+	});
+
 	var f=d.getElementsByTagName(s)[0],
 		j=d.createElement(s),
-		dl=l!='dataLayer'?'&l='+l:'';
-		j.async=true;
-		j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		dl=l!='dataLayer' ? '&l='+l : '';
+
+	j.async = true;
+	j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+
+	f.parentNode.insertBefore(j,f);
+
 })(window,document,'script','dataLayer','GTM-PDWTVMM2');
-//End Google Tag Manager
 
-//Ancien script, potentiellement à changer
-(function () {
-  const GA_ID = 'G-453KG168EG';
-  const CONSENT_KEY = 'consent_analytics';
-
-  // Charger la librairie GA (une seule fois)
-  if (!document.getElementById('ga4-script')) {
-    const script = document.createElement('script');
-    script.id = 'ga4-script';
-    script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_ID;
-    document.head.appendChild(script);
-  }
-
-  window.dataLayer = window.dataLayer || [];
-  window.gtag = function () {
-    dataLayer.push(arguments);
-  };
-  gtag('js', new Date());
-
-  window.enableAnalytics = function () {
-    try {
-      if (localStorage.getItem(CONSENT_KEY) !== 'granted') return;
-    } catch (e) {
-      return;
-    }
-
-    if (window.__gaInitialized) return;
-    window.__gaInitialized = true;
-
-    gtag('config', GA_ID, {
-      anonymize_ip: true
-    });
-  };
-
-})();
+/*
+|--------------------------------------------------------------------------
+| Activation analytics après consentement
+|--------------------------------------------------------------------------
 */
+
+window.enableAnalytics = function(){
+
+	gtag('consent', 'update', {
+		'analytics_storage': 'granted'
+	});
+
+	dataLayer.push({
+		event: 'analytics_consent_granted'
+	});
+};
