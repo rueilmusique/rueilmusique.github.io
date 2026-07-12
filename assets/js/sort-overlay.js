d76e6309ee8258252812919a491f5ab50629edef
TryAgain911 <emulateurs@hotmail.fr>
2026-07-12 19:50:59 +0200

    anchor link immediate overlay

--- assets/js/sort-overlay.js
+++ assets/js/sort-overlay.js
@@ -3,42 +3,54 @@ const overlayText = document.getElementById("overlay-text");
 const overlayImg = document.getElementById("overlay-img");
 const close = document.getElementById("close");
 
+function ouvrirOverlay(produit) {
+
+    const detail = produit.querySelector(".produit-detail");
+    const picture = produit.querySelector("picture");
+
+    overlayText.innerHTML = detail.innerHTML;
+    overlayImg.replaceChildren(picture.cloneNode(true));
+	
+	overlay.dataset.produitId = produit.id;
+	
+	history.replaceState(null, "", `#${produit.id}`);
+	
+	const boutonPartager = overlayText.querySelector(".boutonPartager");
+	
+    if (boutonPartager) {
+        boutonPartager.addEventListener("click", async () => {
+            await navigator.clipboard.writeText(location.href);
+            boutonPartager.textContent = "✅ Lien copié !";
+            setTimeout(() => {
+				boutonPartager.textContent = "🔗 Copier le lien";
+			}, 1500);
+			
+		});
+	}
+	overlay.classList.remove("hidden");
+}
+
+function fermerOverlay() {
+	overlay.classList.add("hidden");
+	history.replaceState(null, "", location.pathname);
+}
+
 document.querySelectorAll(".produit").forEach(produit => {
-
 	produit.addEventListener("click", () => {
-
-		const detail = produit.querySelector(".produit-detail");
-		const picture = produit.querySelector("picture");
-
-		overlayText.innerHTML = detail.innerHTML;
-		overlayImg.replaceChildren(picture.cloneNode(true));
-		
-		const boutonPartager = overlayText.querySelector(".boutonPartager");
-		if (boutonPartager) {
-			boutonPartager.addEventListener("click", async (e) => {
-				e.stopPropagation(); // évite tout effet de bord
-				const url = new URL(location.href);
-				url.hash = produit.id;
-				navigator.clipboard.writeText(url.href);
-				// const url = `${location.origin}${location.pathname}#${produit.id}`;
-				// await navigator.clipboard.writeText(url);
-				boutonPartager.textContent = "✅ Lien copié !";
-				setTimeout(() => {
-					boutonPartager.textContent = "🔗 Copier le lien";
-				}, 1500);
-			});
-		}
-
-		overlay.classList.remove("hidden");
+		ouvrirOverlay(produit);
 	});
 });
 
-close.addEventListener("click", () => {
-	overlay.classList.add("hidden");
-});
-
+close.addEventListener("click", fermerOverlay);
 overlay.addEventListener("click", (e) => {
 	if (e.target === overlay) {
-		overlay.classList.add("hidden");
+		fermerOverlay();
 	}
 });
+
+if (location.hash) {
+	const produit = document.querySelector(location.hash);
+	if (produit) {
+		ouvrirOverlay(produit);
+	}
+}
